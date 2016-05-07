c6bb961e70718cd0111a6f385aa33cf339b80b53
CORDEA <contact@cordea.jp>
2016-05-07 20:57:57 +0900

    fix license

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding:utf-8
 #
-# Copyright [2015] [Yoshihiro Tanaka]
+# Copyright 2015-2016 Yoshihiro Tanaka
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
